14f6d9d8b1ed7e7de88702c00d1d667e0ab5c18e
MANISH WALIA <78153901+manish15walia@users.noreply.github.com>
2022-11-01 22:11:20 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,27 +1,22 @@
                                                                                                          
 const canvas = document.querySelector('canvas')
 
-const ctx = canvas.getContext('2d'); //2d me karege kaam.
-//ctx var me sare properties store krvali.
+const ctx = canvas.getContext('2d'); 
 // ctx.fillStyle='black'
 // ctx.fillRect(10,20,50,40)
- //fillrect structure create krne m help krte.isme  4 parameter pass hote : x,y coordinate , height width
- //ek rect box create kra apne canvas me.color decide kra black. uska shape kesa hoga orr pure screen p uske position kya hoge. 0,0, means starting from initial starting pt 0,0 hona chaye
- //x axis y axis k coordinate 0,0.
- //last me 50,40 : height or width decide kr rha. (Size)
+
            
  let square = 50
  var snakeCell = [[0,0]]
  let bHight = 600
  let bWidth = 1000
  let direction = 'right' //initial dirn right k trf 
-//ek cell add hota rhga. 2d arr bnya qki dynamic chaye.
-//ek function : jo canvas k andar snake ke cells ko banyegaa.
+
 let gameOver = false 
 let foodG = generateRandomCell()
 let score = 0;
 
-document.addEventListener('keydown', function(e){ //e return krga konsa button click kra or uske andr konse properties h
+document.addEventListener('keydown', function(e){ 
 // console.log(e)
 if(e.key==='ArrowRight'){
     direction='right'                                     
@@ -35,11 +30,9 @@ else if(e.key==='ArrowDown'){
 else{
     direction='up'
 }
-}) //keydown hone p kch func run kre.
+}) 
 function update(){
-    //initally snake bnega usme kch changes lane h to is func k andr hoga.
-    // console.log("hello update")
-
+ 
 
 
 headX=snakeCell[snakeCell.length-1][0]
@@ -90,19 +83,15 @@ if(newX==foodG[0] && newY == foodG[1]){
            
 else{
 
- //isme 2 val pass krge coordinate.
 
-snakeCell.shift() //ye age se new new cell ko remove krta rhga
-}
-
-//newX :  headX me ek val add krdege sqaure k height width
-//headX cell k last wla point decide krga.
+snakeCell.shift() 
 }
 
 
-//ek or func jo snake ko draw karega.
+}
+
 function generateRandomCell(){
-    return[Math.round(Math.random()*(bWidth-square)/square)*square, //to generate food and hamesha random coordinate generate hone wo 50 k multiple hoge(jitna snake k cell  k size h)
+    return[Math.round(Math.random()*(bWidth-square)/square)*square, 
     Math.round(Math.random()*(bHight-square)/square)*square
     ];
 
@@ -131,12 +120,12 @@ function draw(){
 }
 let id = setInterval(function(){
     update()
-    draw() // dono ko call har 100 millisec bad call.
-},100) //2 paramter : call back func or time kitne tym k liye call hoga //set Interval : dono func ko har 100 milisec bad move krga.
+    draw() 
+},100) 
 
 
  
-//Math random func humesha 0 or 1 k bich jo value dega wo hi hoga.
+
 
 // console.log(Math.random()*10)
 
